5e26d5735e297b5289faf6bab407ae77ab494866
Manoj <khamitkarmanoj@gmail.com>
2023-06-27 12:51:42 +0530

    Single_Dimensional_Array

---
+++ Arrays/Color_03.java
@@ -0,0 +1,14 @@
+// Single Dimensional array
+// looping thorgh Array using For loop.
+
+public class Color_03{
+    public static void main(String[] args){
+    // Creating an Array
+    int num[]={1,2,3,4,5,6,7,8,9};
+    int len = num.length;
+    // using for loop
+    for(int i=0;i<len;i++){ 
+    System.out.println(num[i]);
+    }
+    }
+}
