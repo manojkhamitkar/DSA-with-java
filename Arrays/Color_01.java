3cd6ecba06b2236c4cba09ef4c090c7f8a6e5472
Manoj <khamitkarmanoj@gmail.com>
2023-06-27 11:05:40 +0530

    Single_Dimensional_Array

---
+++ Arrays/Color_01.java
@@ -0,0 +1,20 @@
+// Single Dimensiional Array
+
+public class Color_01
+{
+    public static void main(String[] args){
+    // To create an Single-Dimension array:
+    int[] myArray ={1,2,3,4,5};
+    // first element
+    System.out.println(myArray[0]);
+    // second element
+    System.out.println(myArray[1]);
+    // third element
+    System.out.println(myArray[2]);
+    // fourth element
+    System.out.println(myArray[3]);
+    // fifth element
+    System.out.println(myArray[4]);
+    }
+}
+
