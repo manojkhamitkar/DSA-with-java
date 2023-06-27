1e238d289afa66b92bf470d63e42e6302cbd16fb
Manoj <khamitkarmanoj@gmail.com>
2023-06-27 11:45:01 +0530

    Single_Dimensional_Array

---
+++ Arrays/Color_04.java
@@ -0,0 +1,10 @@
+// Single Dimensional Array
+// looping through array using For each loop
+public class Color_04 {
+    public static void main(String[] args){
+        String[] colours = {"red","blue","yellow"};
+        // To print the elements in the console
+        for(String colour : colours)
+        System.out.print(colour + ", ");
+    }
+}
