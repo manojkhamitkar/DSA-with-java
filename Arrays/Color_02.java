963c4e9fc757e7b5507776bbc7775eaa7091eac4
Manoj <khamitkarmanoj@gmail.com>
2023-06-27 12:35:52 +0530

    Single_Dimensional_Array

---
+++ Arrays/Color_02.java
@@ -0,0 +1,10 @@
+// Single Dimensional Array
+// Length of array
+
+public class Color_02{
+    public static void main(String[] args){
+        String[] colours = {"red","blur","yello","orange"};
+        int length = colours.length;
+        System.out.println("Length is "+length);
+    }
+}
