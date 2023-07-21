87bd90fa4b5719e23daf8de140a0d78f72e15eec
Manoj <khamitkarmanoj@gmail.com>
2023-07-21 20:32:14 +0530

    The first index of repeated value

--- Arrays/Practice_09.java
+++ Arrays/Practice_09.java
@@ -1,28 +1,26 @@
-/*Find the second-larget value in the given array. */
-
 import java.util.*;
 import java.lang.*;
 public class Practice_09 {
-    public static int secondLargestValue(int[] arr){
-        int n= a.length;
-        int max =Interger.MIN_VALUE;
-        int smax=Interger.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            if(max<a[i])
-            max=a[i];
-        }
-        for(int i=0;i<n;i++){
-            if(a[i]!=max)
-            {
-                if(a[i]>smax)
-                smax=a[i];
+    public static int firstRepeated(int[] a,int n) {
+        for(int i =0;i<n;i++){
+            int curr = a[i];
+            for(int j = i+1;j < n;j++){
+                if(a[j]==curr) return i+1;
             }
         }
-        return smax;
-
+        return -1;
     }
     public static void main(String[] args) {
-        int a[]={21,7,3,21,5,7,5};
-        System.out.println(secondLargestValue(a));
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array size : ");
+        n=sc.nextInt();
+        int[] a =new int[30];
+        System.out.println("Enter "+n+" size");
+        for(int i=0;i<n;i++){
+            a[i] =sc.nextInt();
+        }
+        int ans=firstRepeated(a,n);
+        System.out.println(ans);
     }
 }
