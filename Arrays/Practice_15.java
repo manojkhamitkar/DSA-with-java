3ddcc3cee11bb8b40543657b118e653016860ad0
Manoj <khamitkarmanoj@gmail.com>
2023-08-13 22:50:11 +0530

    Prefix Sum

--- Arrays/Practice_15.java
+++ Arrays/Practice_15.java
@@ -2,9 +2,11 @@
 
 import java.io.*;
 class Practice_15 {
+    // Fills prefix sum array
     static void fillPrefixSum(int arr[], int n,int prefixSum[])
     {
         prefixSum[0]= arr[0];
+    // Adding present element with previous element
         for(int i =  1; i< n; i++)
         prefixSum[i] = prefixSum[i-1]+arr[i];
     }
@@ -12,6 +14,8 @@ class Practice_15 {
         int arr[] = {10,4,16,20};
         int n = arr.length;
         int prefixSum[] = new int[n];
+
+        // Function call
         fillPrefixSum(arr, n, prefixSum);
         for(int i =0;i<n;i++)
         System.out.print(prefixSum[i] + " ");
