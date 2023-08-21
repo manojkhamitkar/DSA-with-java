45dcc901c205ef4b8b80d0e934653853a6139a74
Manoj kumar <khamitkarmanoj@gmail.com>
2023-08-21 20:39:02 +0530

    Exist of number in matrix or not

---
+++ 2D-Arrays/Practice_1.java
@@ -0,0 +1,35 @@
+/* Check if an element x exists in the given matrix or not. If it does not exist, return -1, else return its
+row and column index.*/
+
+import java.util.Scanner;
+public class Practice_1 
+{
+    public static void printArray(int[][] arr){
+        for(int i=0; i < arr.length; i++){
+            for(int j =0; j <arr[i].length;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+         System.out.println();  
+        }
+         
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows ");
+        int r = sc.nextInt();
+        System.out.println("Enter number of columns ");
+        int c = sc.nextInt();
+        int arr[][] = new int[r][c];
+        System.out.println("Enter elements of matrix ");
+         for(int i=0; i < r; i++){
+            for(int j =0; j <c;j++){
+                arr[i][j] = sc.nextInt();
+            }
+           
+        }
+           printArray(arr);
+        }
+
+    }
+    
+
