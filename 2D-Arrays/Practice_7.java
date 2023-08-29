8687ee1d87bfc913a527cb31961f870ec4c346e7
Manoj kumar <khamitkarmanoj@gmail.com>
2023-08-29 22:55:41 +0530

    a square matrix, turn it by 90 degrees in a clockwise direction without using any extra space.

--- 2D-Arrays/Practice_7.java
+++ 2D-Arrays/Practice_7.java
@@ -2,22 +2,22 @@
 
 import java.util.Scanner;
 public class Practice_7 {
-    static void printArray(int[][] arr) {
+    public static void printArray(int[][] arr) {
         for(int i = 0; i < arr[i].length; i++){
             for(int j = 0; j < arr.length; j++){
-                System.out.println(arr[i][j] + " ");
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
-    public static void rotate(int arr[][], int N){
-        for (int i = 0; i < N/2; i++){
-            for(int j = i; j < N-i-1; j++){
+    public static void rotate(int arr[][], int n){
+        for (int i = 0; i < n/2; i++){
+            for(int j = i; j < n-i-1; j++){
                 int temp = arr[i][j];
-                arr[i][j] = arr[N-1-j][i];
-                arr[N-1-i][i] = arr[N-1-i][N-1-j];
-                arr[N-1-j][N-1-j] = arr[j][N-1-i];
-                arr[j][N-1-i] = temp;
+                arr[i][j] = arr[n-1-j][i];
+                arr[n-1-i][i] = arr[n-1-i][n-1-j];
+                arr[n-1-j][n-1-j] = arr[j][n-1-i];
+                arr[j][n-1-i] = temp;
             }
         }
     }
@@ -26,15 +26,16 @@ public class Practice_7 {
         System.out.println("Enter number of rows and columns");
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int[][] arr = new int[n][m];
+        int[][] arr1 = new int[n][m];
         System.out.println("Enter "+n*m+" elements");
         for(int i = 0; i < n; i++){
             for(int j =0; j < m; j++){
-                arr[i][j] = sc.nextInt();
+                arr1[i][j] = sc.nextInt();
             }
         }
-        printArray(arr);
-        rotate(arr, n);
+        
+        rotate(arr1, n);
+        printArray(arr1);
     }
     
 }
