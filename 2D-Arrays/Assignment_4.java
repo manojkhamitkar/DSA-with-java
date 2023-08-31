202544ad5a8df5446d8e385bddc2987ba11ea169
Manoj kumar <khamitkarmanoj@gmail.com>
2023-08-31 22:59:52 +0530

    Toeplitz matrix or not

---
+++ 2D-Arrays/Assignment_4.java
@@ -0,0 +1,15 @@
+/* Given a n*m matrix, return an array of elements containing diagonal traversal of the matrix. */
+
+import java.util.*;
+public class Assignment_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 3 , m = 3;
+        int[][] arr = {{1,2,3},
+                       {4,5,6},
+                       {7,8,9}};
+
+        }
+    }
+    
+
