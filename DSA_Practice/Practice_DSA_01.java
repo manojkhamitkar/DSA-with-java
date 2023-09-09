9a3c8ac7096d3abb9d8cc40a9f46e9351ffdcd38
Manoj kumar <khamitkarmanoj@gmail.com>
2023-09-09 23:56:04 +0530

    identity PossibleWords

--- DSA_Practice/Practice_DSA_01.java
+++ DSA_Practice/Practice_DSA_01.java
@@ -1,25 +1,27 @@
+/* Identify possible words: Detective Bakshi while solving a case stumbled upon a letter which
+had many words whose one character was missing ie. one character in the word was replaced
+by an underscore. For e.g. “Fi_ er”. He also found thin strips of paper which had a group of
+words separated by colons, for e.g. “Fever:filer:Filter: Fixer:fiber:fibre:tailor:offer”. He could
+figure out that the word whose one character was missing was one of the possible words from
+the thin strips of paper. Detective Bakshi has approached you (a computer programmer)
+asking for help in identifying the possible words for each incomplete word.
+You are expected to write a function to identify the set of possible words
+The function identity PossibleWords takes two strings as input,
+input1 contains the incomplete word, and
+input2 is the string containing a set of words separated by colons
+The function is expected to find all the possible words from input2 that can replace the
+incomplete word input1, and then return the final output string in the format specified below
+See below examples carefully to understand the format of input1, input2 and output
+Example1:
+input1= "Fi_er”
+input2=”Fever:filer:Filter: Fixer:fiber:fibre:tailor:offer”
+output= "FILER:FIXER FIBER” */
 package DSA_Practice;
 import java.util.*;
 public class Practice_DSA_01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n1 = 0;
-        int n2 = 1;
-        int n=5, fib;
-       // int fib;
-        //System.out.println("Enter a number : ");
-        //int n = sc.nextInt();
-        for(int i = 0; i < 5 ; i++){
-        System.out.print(n1);
-        fib = n1 + n2;
-        n1 = n2;
-        n2 = fib;
-          // System.out.print(fib);
-            //fib++;
-                    
-        }
-        //System.out.println();
-        //  fib++;
+       
          
     }
     
