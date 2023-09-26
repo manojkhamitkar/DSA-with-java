6484513e9453c3048260f572d0e1c1654c30bacb
Manoj kumar <khamitkarmanoj@gmail.com>
2023-09-26 21:02:38 +0530

    Spiral Order of Matrix

---
+++ 2D-Arrays/Practice_8.java
@@ -0,0 +1,70 @@
+// Returning spiral order elements from the matrix .
+import java.util.*;
+public class Practice_8 {
+    static void printMatrix(int[][] matrix){
+        for(int i = 0 ; i < matrix.length; i++){
+            for(int j = 0 ; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static void printSpiralOrder(int[][] matrix , int r , int c){
+        int topRow= 0 , bottowRow = r-1, leftCol = 0 , rightCol = c-1;
+        int totalelement = 0;
+        
+        while(totalelement < r * c){
+
+            // toprow -> leftCol to rightCol
+            for(int j = leftCol ; j < rightCol; j++){
+                System.out.print(matrix[topRow][j] + " ");
+                totalelement++;
+            }
+            topRow++;
+
+            // rightCol -> topRow to bottomRow
+            for(int i = topRow; i < bottowRow; i ++){
+                System.out.print(matrix[i][rightCol] + " ");
+                totalelement++;
+            }
+            rightCol--;
+
+            //bottomrow -> rightCol to leftCol
+            for(int j = rightCol ; j >= leftCol; j --){
+                System.out.print(matrix[bottowRow][j] + " ");
+                totalelement++;
+            }
+            bottowRow--;
+
+            // leftCol -> bottomCol to topCol
+            for(int i = bottowRow; i >= topRow ; i--){
+                System.out.print(matrix[i][leftCol] + " ");
+                totalelement++;
+            }
+            leftCol++;
+        }
+
+       
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows & columns");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+        int totalelement = r * c;
+        System.out.println("Enter "+totalelement+" elements");
+        for(int i = 0 ; i < r; i ++){
+            for(int j = 0 ; j < c; j ++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.print("Input Matrix");
+        printMatrix(matrix);
+
+        System.out.println("Spiral Matrix");
+        printSpiralOrder(matrix, r, c);
+    }
+    
+}
---
+++ DSA_Practice/Practice_DSA_22.java
@@ -0,0 +1,26 @@
+/* Detective Buckshee junior has been approached by the shantiniketankids society for help in
+finding the password to the games complex. After hearing the scenario, detective Buckshee
+junior realizes that he will need a programmer's support. He contacts you requests your help.
+Please help the detective by writing a function to generate the password.
+The scenario is as below Five numbers are available with the kids.
+These numbers are either stable or unstable
+A numbers is stable if each of its digit occur the same number of times, i.e the frequency of each digit
+in the number is the same.
+For e.g:2277,4004,11,23,583835,1010 are examples of stable numbers.
+Similarly, A number is unstable if the frequency of each digit in the number is NOT the same. For
+eg:221,4314,101,233,58135,101 are examples of unstable numbers.
+The password can be found as below i.e Password=Maximum of all stable numbers - Minimum of all
+Unstable numbers
+Assuming that the five numbers are passed to a function as input1, input2, input3, input4, input5.
+Complete the function to find and return the password.
+For example:
+If input1=12,input2=1313,input3=122,input4=678 and input5=898 , we see that there are THREE stable
+numbers 12,1313 and 678 and
+TWO unstable numbers 122 and 898
+So, the Password should be=Maximum of all stable numbers - Minimum of all Unstable
+numbers=1313-122=1191 */
+package DSA_Practice;
+
+public class Practice_DSA_22 {
+    
+}
