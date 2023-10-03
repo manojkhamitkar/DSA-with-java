917107adfdaa2bea54d6478f3c3191ee8d0f550d
Manoj kumar <khamitkarmanoj@gmail.com>
2023-10-03 23:54:05 +0530

    Traversing a matrix boundary

--- 2D-Arrays/Practice_2DArray_11.java
+++ 2D-Arrays/Practice_2DArray_11.java
@@ -12,14 +12,37 @@ public class Practice_2DArray_11 {
 
     }
     static void matrixBoundary(int[][] matrix, int r, int c){
-        // toprow = rightcol to leftcol
+        int toprow = 0 , bottomrow = r-1, leftcol = 0, rightcol = c-1; 
+        // toprow = leftcol to rightcol
+        for(int j = leftcol; j < rightcol; j++ ){
+           System.out.println( matrix[toprow][j]);
+        }
         // leftcol = toprow to bottomrow
+        for(int i = toprow; i < bottomrow; i++ ){
+             System.out.println( matrix[i][leftcol]);
+        }
         // bottomrow = leftcol to rightcol
+        for(int j = leftcol; j < rightcol; j-- ){
+             System.out.println( matrix[bottomrow][j]);
+        }
         // rightcol = bottomrow to toprow
+        for(int i= bottomrow; i < toprow; i-- ){
+             System.out.println( matrix[i][rightcol]);
+        }
         
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int r = sc.nextInt();
+        int c= sc.nextInt();
+        int[][] matrix = new int[r][c];
+        System.out.println("Enter "+r*c +" elements");
+        for(int i = 0 ; i < r; i ++){
+            for(int j = 0 ; j < c; j ++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        matrixBoundary(matrix, r, c);
+        printMatrix(matrix);
     }
 }
