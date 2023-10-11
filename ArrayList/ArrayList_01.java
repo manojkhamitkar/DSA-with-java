6927477ab169c85687edd951fcf778f4b92942a6
Manoj kumar <khamitkarmanoj@gmail.com>
2023-10-11 22:57:58 +0530

    Reverse of ArrayList

--- ArrayList/ArrayList_01.java
+++ ArrayList/ArrayList_01.java
@@ -18,6 +18,14 @@ public class ArrayList_01 {
         list.add(13);
         System.out.println("Before reverse: ");
         print(list);
+        System.out.println("");
+        for (int i = 0; i < list.size() / 2; i++) {
+            Integer t = list.get(i);
+            list.set(i, list.get(list.size() - i - 1));
+            list.set(list.size() - i - 1, t);
+        }
+        System.out.println("After reverse: ");
+        print(list);
 
     }
 
