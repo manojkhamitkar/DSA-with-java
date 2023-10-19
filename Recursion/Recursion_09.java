d9a6d9f3dcb4276acd8c7686d50934ac1d70b2f2
Manoj kumar <khamitkarmanoj@gmail.com>
2023-10-19 21:56:08 +0530

    Maximum Value in array using recursion

--- Recursion/Recursion_09.java
+++ Recursion/Recursion_09.java
@@ -1,3 +1,25 @@
-public class Recursion_09 {
 
+/* 1 Print the max value of the array  [ 3, 10, 3, 2, 5] using recursion. */
+import java.util.*;
+import java.io.*;
+
+public class Recursion_09 {
+    static int maximumElement(int[] arr, int n, int idx) {
+
+        // Base case
+        if (idx == n - 1)
+            return arr[n - 1];
+
+        // Recursive call
+        int maxOfLaterIndices = maximumElement(arr, n, idx + 1);
+        int maxVal = Math.max(arr[idx], maxOfLaterIndices);
+        return maxVal;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 10, 3, 2, 5 };
+        int n = arr.length;
+        System.out.println(maximumElement(arr, n, 0));
+
+    }
 }
