7e0c9b2baeb096c2b01cb09e1a0261afab0eb9a0
Manoj kumar <khamitkarmanoj@gmail.com>
2023-10-28 14:15:45 +0530

    First commit

---
+++ Arrays/GFG_04.java
@@ -0,0 +1,5 @@
+/* Given an array arr[] of integers, segregate even and odd numbers in the array such 
+that all the even numbers should be present first, and then the odd numbers. */
+public class GFG_04 {
+
+}
