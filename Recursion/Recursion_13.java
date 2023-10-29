248c2831a4ab736a632d9dfb2c6c46d699f1c4fa
Manoj kumar <khamitkarmanoj@gmail.com>
2023-10-29 23:50:36 +0530

    GCD Using Euclid Algorithm (by Subtracting)

--- Recursion/Recursion_13.java
+++ Recursion/Recursion_13.java
@@ -1,1 +1,21 @@
 
+/* Find the greate9t common divi9or (GCD) or HCF (Highe9t Common Factor) for the given two number9 by u9ing
+the recur9ive function. --> GCD by Euclidean Algorithm(by Subtraction) */
+import java.util.*;
+
+public class Recursion_13 {
+    static int gcd(int a, int b) {
+        if (a == 0 || b == 0) // base case
+            return a;
+        if (a > b)
+            return gcd(a - b, b);
+        return gcd(a, b - a);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 54, b = 72;
+        System.out.println(gcd(a, b));
+    }
+}
