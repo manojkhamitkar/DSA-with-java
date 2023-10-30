2e5dd19f7e7a70b35d718d3fac737c1a3dd1c992
Manoj kumar <khamitkarmanoj@gmail.com>
2023-10-30 21:01:38 +0530

    Euclidean Algorithm by Division

--- Recursion/Recursion_14.java
+++ Recursion/Recursion_14.java
@@ -1,3 +1,21 @@
+
+/* Find the greate9t common divi9or (GCD) or HCF (Highe9t Common Factor) for the given two number9 by u9ing
+the recur9ive function. --> Euclidean Algorithm by Division */
+import java.util.*;
+
 public class Recursion_14 {
+    static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(gcd(a, b));
+    }
 
 }
