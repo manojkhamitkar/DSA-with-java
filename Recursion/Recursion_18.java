f02d91a92a7751359830fc014b6897f8f1b4dc65
Manoj kumar <khamitkarmanoj@gmail.com>
2023-11-07 21:11:16 +0530

    Maximum Value in Array

--- Recursion/Recursion_18.java
+++ Recursion/Recursion_18.java
@@ -4,13 +4,23 @@ import java.util.*;
 import java.io.*;
 
 public class Recursion_18 {
+    // Apporach -> 1
     static int maxValue(int[] arr, int n, int idx) {
-        if (idx == n - 1)
+        if (idx == n - 1) // Base Case
             return arr[n - 1];
-        int maxofLaterIndices = maxValue(arr, n, idx + 1);
-        int maxVal = Math.max(arr[idx], maxofLaterIndices);
+        int maxofLaterIndices = maxValue(arr, n, idx + 1); // Self Work
+        int maxVal = Math.max(arr[idx], maxofLaterIndices); // Recursive Work
         return maxVal;
 
+    }
+
+    // Apporach -> 2
+    static int maxInArray(int[] arr, int idx) {
+        if (idx == arr.length - 1) // Self Work
+            return arr[idx];
+
+        int smallAns = maxInArray(arr, idx + 1);
+        return Math.max(smallAns, arr[idx]); // Recursive Work
     }
 
     public static void main(String[] args) {
@@ -20,8 +30,8 @@ public class Recursion_18 {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        // maxValue(arr, n, 0);
-        System.out.println(maxValue(arr, n, 0));
+        System.out.println("Apporch-1 : " + maxValue(arr, n, 0));
+        System.out.println("Apporach-2 : " + maxInArray(arr, 0));
 
     }
 
---
+++ Recursion/Recursion_19.java
@@ -0,0 +1,15 @@
+
+/*Find the sum of the elements of the array.  */
+import java.util.*;
+
+public class Recursion_19 {
+    static void sumOfArray(int[] arr, int idx) {
+
+    }
+
+    public static void main(String[] args) {
+        // int[] arr = {1,2,3,4};
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
