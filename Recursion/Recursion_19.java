eb00ddc9736d618a28f9edd8d1a93cee73843ed3
Manoj kumar <khamitkarmanoj@gmail.com>
2023-11-07 22:33:57 +0530

    Sum of Array Element using recursion

--- Recursion/Recursion_19.java
+++ Recursion/Recursion_19.java
@@ -1,15 +1,18 @@
 
 /*Find the sum of the elements of the array.  */
-import java.util.*;
 
 public class Recursion_19 {
-    static void sumOfArray(int[] arr, int idx) {
+    static int sumOfArray(int[] arr, int idx) {
+        if (idx == arr.length) // Base Case
+            return 0;
 
+        int smallAns = sumOfArray(arr, idx + 1); // Recursive work
+        return smallAns + arr[idx]; // Self work
     }
 
     public static void main(String[] args) {
-        // int[] arr = {1,2,3,4};
-        Scanner sc = new Scanner(System.in);
+        int[] arr = { 1, 2, 3, 4 };
+        System.out.println(sumOfArray(arr, 0));
 
     }
 }
