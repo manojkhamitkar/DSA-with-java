f7f198f94a4ef2dada2dbb0e13dab6c495aae57a
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-01 19:05:16 +0530

    Implementation of linked list

--- Linked_list/Linked_list_01.java
+++ Linked_list/Linked_list_01.java
@@ -22,11 +22,18 @@ public class Linked_list_01 {
         b.next = c;// 5 -> 4 -> 1 9 0
         c.next = d;// 5 -> 4 -> 1 -> 9 0
         d.next = e;// 5 -> 4 -> 1 -> 9 -> 0
-        System.out.print(a.data + " ");
-        System.out.print(a.next.data + " ");
-        System.out.print(a.next.next.data + " ");
-        System.out.print(a.next.next.next.data + " ");
-        System.out.print(a.next.next.next.next.data + " ");
+
+        // System.out.print(a.data + " ");
+        // System.out.print(a.next.data + " ");
+        // System.out.print(a.next.next.data + " ");
+        // System.out.print(a.next.next.next.data + " ");
+        // System.out.print(a.next.next.next.next.data + " ");
+
+        Node temp = a;
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
 
     }
 }
