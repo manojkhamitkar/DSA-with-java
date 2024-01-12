a132b75302a0211b809ccd22a1f254d32dbe9048
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-12 17:18:20 +0530

    Bubble Sorting

--- Sorting_Algorithm/BubbleSorting.java
+++ Sorting_Algorithm/BubbleSorting.java
@@ -9,15 +9,19 @@ public class BubbleSorting {
 
         // n-1 iterations/passes
         for (int i = 0; i < n - 1; i++) {
+            boolean flag = false;
             for (int j = 0; j < n - 1 - i; j++) {
 
                 if (arr[j] > arr[j + 1]) {
+                    flag = true;
                     // swap a[j+1] and a[j]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
+            if (!flag)
+                return;
         }
 
     }
---
+++ Sorting_Algorithm/SelectSorting.java
@@ -0,0 +1,5 @@
+package Sorting_Algorithm;
+
+public class SelectSorting {
+
+}
