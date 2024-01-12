962a998c43a5097bdcadd2b81fa7bd183ecd808c
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-12 11:14:34 +0530

    Bubble Sort

--- Sorting_Algorithm/BubbleSorting.java
+++ Sorting_Algorithm/BubbleSorting.java
@@ -8,21 +8,15 @@ public class BubbleSorting {
         int n = arr.length;
 
         // n-1 iterations/passes
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
 
-                /*
-                 * last i elements are already at correct sorted positions,
-                 * so no need to check them
-                 */
                 if (arr[j] > arr[j + 1]) {
-                    // swap - arr[j], arr[j+1]
-                    int swap = arr[j];
+                    // swap a[j+1] and a[j]
+                    int temp = arr[j];
                     arr[j] = arr[j + 1];
-                    arr[j + 1] = swap;
-
+                    arr[j + 1] = temp;
                 }
-
             }
         }
 
@@ -31,8 +25,8 @@ public class BubbleSorting {
     public static void main(String[] args) {
         int[] arr = { 2, 6, 9, 2, 7, 0, 7 };
         bubbleSort(arr);
-        for (int i : arr) {
-            System.out.print(i + " ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
 
     }
