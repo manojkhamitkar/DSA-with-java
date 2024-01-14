8b3449dd3dd3aecab184cd98473c4b22a57dfa15
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-14 22:54:11 +0530

    Selection Sort

---
+++ Sorting_Algorithm/QuickSorting.java
@@ -0,0 +1,15 @@
+// Quick Sorting
+package Sorting_Algorithm;
+
+import java.util.*;
+
+public class QuickSorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+}
