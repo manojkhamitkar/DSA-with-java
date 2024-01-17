f67962fe4330ef40f12422cc5339fe2b397c345f
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-17 22:23:10 +0530

    Count Sort

--- Sorting_Algorithm/CountSort.java
+++ Sorting_Algorithm/CountSort.java
@@ -12,7 +12,7 @@ public class CountSort {
     }
 
     static void basicCountSort(int[] arr) {
-        int max = findMax(arr);
+        int max = findMax(arr); // Find the largest element of the array
         int[] count = new int[max + 1];
         for (int i = 0; i < arr.length; i++) {
             count[arr[i]]++;
@@ -35,19 +35,23 @@ public class CountSort {
     static void countSort(int[] arr) {
         int n = arr.length;
         int[] output = new int[n];
-        int max = findMax(arr);
+        int max = findMax(arr); // Find the largest element of the array
         int[] count = new int[max + 1];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) { // Make frequency array
             count[arr[i]]++;
         }
+        // Find the index of each element in the original array and put it in output
+        // array
         for (int i = 1; i < count.length; i++) {
             count[i] += count[i - 1];
         }
+        // copy all elements of output to array
         for (int i = n - 1; i >= 0; i--) {
             int idx = count[arr[i]] - 1;
             output[idx] = arr[i];
             count[arr[i]]--;
         }
+        // copy all elements of output to original array
         for (int i = 0; i < n; i++) {
             arr[i] = output[i];
         }
