0dffbb998ffe8fd7541818f935117abb45a27a5f
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-24 22:27:07 +0530

    ll

--- Linked_list/Practice/Practice_03.java
+++ Linked_list/Practice/Practice_03.java
@@ -1,3 +1,6 @@
+/* Given the heads of two singly linked-lists headA and headB, return the value of the node at which
+the two lists intersect. If the two linked lists have no intersection at all, return -1.Note: the values in each
+node are distinct. */
 package Linked_list.Practice;
 
 public class Practice_03 {
