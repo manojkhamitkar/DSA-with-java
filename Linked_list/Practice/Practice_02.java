6428c4a454bf8f457c3105eeeb9f9dbb26ee0509
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-24 22:21:08 +0530

    remove the k-th node from the end of the list and return its head

--- Linked_list/Practice/Practice_02.java
+++ Linked_list/Practice/Practice_02.java
@@ -6,17 +6,51 @@ package Linked_list.Practice;
 public class Practice_02 {
     public static class Node {
         int data;
+        Node next;
 
         Node(int data) {
             this.data = data;
         }
     }
 
-    static Node deleteKthNode(Node node) {
+    static void deleteKthNode(Node head, int n) {
+        Node slow = head;
+        Node fast = head;
+        for (int i = 1; i <= n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
 
     }
 
+    static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(4);
+        Node c = new Node(1);
+        Node d = new Node(6);
+        Node e = new Node(9);
+        Node f = new Node(12);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        display(a);
+        deleteKthNode(a, 2);
+        display(a);
 
     }
 
