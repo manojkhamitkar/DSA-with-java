f9fd229e85ee78e84e650b2b75ca8bbfa94b666e
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-24 21:15:47 +0530

    remove the k-th node from the end of the list

--- Linked_list/Practice/Practice_02.java
+++ Linked_list/Practice/Practice_02.java
@@ -1,5 +1,23 @@
+/* Given the head of a linked list, remove the k-th node from the end of the list and return its head.>
+Note: It is guaranteed that k <= length Kf the linked list.*/
 package Linked_list.Practice;
 
+//import java.util.*;
 public class Practice_02 {
+    public static class Node {
+        int data;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static Node deleteKthNode(Node node) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
