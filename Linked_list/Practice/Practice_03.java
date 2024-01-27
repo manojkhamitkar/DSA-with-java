2b17a42fdd53659bc5b1ac039935ddc12bdfa155
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-27 16:36:36 +0530

    Intersection point in linked list

--- Linked_list/Practice/Practice_03.java
+++ Linked_list/Practice/Practice_03.java
@@ -6,5 +6,47 @@ package Linked_list.Practice;
 import java.util.*;
 
 public class Practice_03 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        int lengthA = 0;
+        int lengthB = 0;
+        while (tempA != null) {
+            lengthA++;
+            tempA = tempA.next;
+        }
+        while (tempB != null) {
+            lengthB++;
+            tempB = tempB.next;
+        }
+        tempA = headA;
+        tempB = headB;
+        if (lengthA > lengthB) {
+            int steps = lengthA - lengthB;
+            for (int i = 1; i <= steps; i++) {
+                tempA = tempA.next;
+            }
+        } else {
+            int steps = lengthB - lengthA;
+            for (int i = 1; i <= steps; i++) {
+                tempB = tempB.next;
+            }
+        }
+        while (tempA != tempB) {
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+        return tempA;
+    }
 
 }
