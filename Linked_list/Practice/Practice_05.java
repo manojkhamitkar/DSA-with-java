0b90146b3e5ad13bd898331c6a0b6d591ac1328c
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-01-29 21:50:01 +0530

    Reverse linked list using 3 pointers

---
+++ Linked_list/Practice/Practice_05.java
@@ -0,0 +1,53 @@
+// Reverse of Linked list using 3 pointers form.
+package Linked_list.Practice;
+
+public class Practice_05 {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node reverse(Node head) {
+        Node curr = head;
+        Node Next = null;
+        Node prev = null;
+        while (curr != null) {
+            Next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = Next;
+        }
+        return prev;
+
+    }
+
+    public static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        Node a = new Node(1);
+        Node b = new Node(8);
+        Node c = new Node(4);
+        Node d = new Node(10);
+        Node e = new Node(24);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        display(a);
+        a = reverse(a);
+        display(a);
+    }
+
+}
