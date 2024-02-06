99f067c2f740f9eee349c36638763c6c2a7f3f6b
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-02-06 22:28:12 +0530

    Deletion of Duplicates in linked list

--- Linked_list/Practice/Practice_09.java
+++ Linked_list/Practice/Practice_09.java
@@ -1,7 +1,63 @@
-/* Given 2 sorted linked lists, merge them into 1 singly linked list such that the resulting list is also
-sorted. */
 package Linked_list.Practice;
 
 public class Practice_09 {
+
+    public static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static Node removeDuplicate(Node head) {
+        Node temp = head;
+        while (temp != null && temp.next != null) {
+            if (temp.next.val == temp.val) {
+                temp.next = temp.next.next;
+            }
+            if (head == null)
+                return null;
+            if (temp.next.val != temp.val) {
+                temp = temp.next;
+            }
+        }
+        return head;
+
+    }
+
+    public static void display(Node head) {
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(1);
+        Node c = new Node(2);
+        Node d = new Node(3);
+        Node e = new Node(3);
+        Node f = new Node(3);
+        Node g = new Node(4);
+        Node h = new Node(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        f.next = g;
+        g.next = h;
+        display(a);
+        a = removeDuplicate(a);
+        display(a);
+
+    }
 
 }
