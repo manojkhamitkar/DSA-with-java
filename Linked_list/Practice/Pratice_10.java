0b1178f4811c935a77854eca8fa362b51d9c9a43
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-02-07 21:58:15 +0530

    ll

--- Linked_list/Practice/Pratice_10.java
+++ Linked_list/Practice/Pratice_10.java
@@ -2,5 +2,40 @@
 package Linked_list.Practice;
 
 public class Pratice_10 {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node next = null;
+        Node curr = head;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static Node isPalindorme(Node head) {
+
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(3);
+        Node e = new Node(2);
+        Node f = new Node(1);
+        System.out.println(isPalindorme(a));
+    
 
 }
