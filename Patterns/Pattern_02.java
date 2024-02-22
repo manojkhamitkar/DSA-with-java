e25d281ca052ad2ddfe230b0ac7546d49fdf222e
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-02-22 21:54:09 +0530

    Patterns

---
+++ Patterns/Pattern_02.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+public class Pattern_02 {
+    public static void main(String[] args) {
+        int n = 11;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || j == n - 1 || i == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
