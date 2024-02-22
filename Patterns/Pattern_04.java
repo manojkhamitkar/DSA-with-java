b1696ef2fbe88a63459b1428d6c2476549476836
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-02-22 21:57:01 +0530

    Patterns

--- Patterns/Pattern_04.java
+++ Patterns/Pattern_04.java
@@ -1,20 +1,20 @@
 package Patterns;
 
-// public class Pattern_04 {
-// public static void main(String[] args) {
-// int n = 11;
-// for (int i = 0; i < n; i++) {
-// for (int j = 0; j < n; j++) {
-// if (j == n / 2) {
-// System.out.print("*");
-// } else {
-// System.out.print(" ");
-// }
+public class Pattern_04 {
+    public static void main(String[] args) {
+        int n = 11;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == n / 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
 
-// }
-// System.out.println();
+            }
+            System.out.println();
 
-// }
-// }
+        }
+    }
 
-// }
+}
