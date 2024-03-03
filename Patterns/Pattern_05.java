b3b0881c2c65314aa906e7c7416c937e18692f77
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-03-03 23:22:59 +0530

    Pattern

--- Patterns/Pattern_05.java
+++ Patterns/Pattern_05.java
@@ -2,6 +2,12 @@ package Patterns;
 
 public class Pattern_05 {
     public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
 
     }
 
