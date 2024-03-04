e4c7d94d794259d9c98b7b0e8a4ae64e9130247f
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-03-04 21:44:13 +0530

    Pattern

--- Patterns/Pattern_06.java
+++ Patterns/Pattern_06.java
@@ -1,5 +1,8 @@
 package Patterns;
 
 public class Pattern_06 {
+    public static void main(String[] args) {
+
+    }
 
 }
--- Patterns/Pattern_07.java
+++ Patterns/Pattern_07.java
@@ -1,5 +1,19 @@
 package Patterns;
 
+import java.util.*;
+
 public class Pattern_07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        // printing a sqaure pattern.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
