9ca749136622d825cebbb824f076d634b64664b4
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-03-05 22:07:37 +0530

    Pattern

--- Patterns/Pattern_08.java
+++ Patterns/Pattern_08.java
@@ -1,5 +1,15 @@
 package Patterns;
 
 public class Pattern_08 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+    }
 
 }
