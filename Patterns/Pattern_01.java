ddb80aa21784806f2a5d8440c89665a54c54e58d
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-03-06 15:05:01 +0530

    Square Pattern

--- Patterns/Pattern_01.java
+++ Patterns/Pattern_01.java
@@ -2,6 +2,7 @@ package Patterns;
 
 public class Pattern_01 {
     public static void main(String[] args) {
+        // Sqaure Pattern.
         int n = 5;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
