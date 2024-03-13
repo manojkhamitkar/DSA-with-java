dee81018f331d140615c1242752e4479ec6beb13
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-03-13 22:56:49 +0530

    reverse numbers right angle triangle pattern

--- Patterns/Pattern_06.java
+++ Patterns/Pattern_06.java
@@ -3,6 +3,7 @@ package Patterns;
 public class Pattern_06 {
     public static void main(String[] args) {
         int n = 5;
+        // reverse numbers right angle triangle pattern.
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n + 1 - i; j++) {
                 System.out.print(j + " ");
