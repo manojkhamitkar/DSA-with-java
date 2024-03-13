d3726a637c761867f87cf6be653a1211ba9ce2ea
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-03-13 22:58:16 +0530

    star square pattern

--- Patterns/Pattern_07.java
+++ Patterns/Pattern_07.java
@@ -7,7 +7,7 @@ public class Pattern_07 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int n = scanner.nextInt();
-        // printing a sqaure pattern.
+        // star sqaure pattern.
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n + 1; j++) {
                 System.out.print("*" + " ");
