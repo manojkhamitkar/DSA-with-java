0dd426a5c1ef180b96a76748f92a390c32a7731d
Manoj Kumar <117462838+kmanoj9347@users.noreply.github.com>
2024-03-13 22:55:07 +0530

    Numbers right angle triangle pattern

--- Patterns/Pattern_05.java
+++ Patterns/Pattern_05.java
@@ -2,6 +2,7 @@ package Patterns;
 
 public class Pattern_05 {
     public static void main(String[] args) {
+        // number right angle triangle pattern.
         for (int i = 1; i <= 4; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
